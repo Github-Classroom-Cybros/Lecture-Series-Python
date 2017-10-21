3e6728b196a2c0b6c3f6aeedb7a6b38e78597f54
Jack McBride <jlgm2@kent.ac.uk>
2017-10-21 19:11:17 +0100

    Add fibonacci script

---
+++ Awesome-Scripts/fibonacci.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
+
+
+n = int(input("Enter a number: "))
+print(fib(n))
